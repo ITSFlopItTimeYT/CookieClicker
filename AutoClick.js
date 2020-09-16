0c17ac8858bd4278f58020840a376174ce3f0c77
ITSFlopItTimeYT <44099468+ITSFlopItTimeYT@users.noreply.github.com>
2020-09-16 16:51:41 -0500

    Create AutoClick.js

---
+++ AutoClick.js
@@ -0,0 +1,3 @@
+var i = 0; var limit = 1000000; function win () { setTimeout(function () { document.getElementById('bigCookie').click(); i++; if (i < limit) { win(); } }, 5)} win();
+
+//click very fast not recommended for slow computers
